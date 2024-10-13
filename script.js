39b877e53a73e37f350a87ad558fbf9c0b4d3b53
OscarMIP <oscar.mataro.epiaedu.cat>
2024-10-13 12:11:15 +0200

    finished (ex1 to ex5)

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-UPDATE
+//UPDATE
 
 // EXERCISE 1: Selecting Elements
 // TODO: Select the element with the ID 'mainTitle' and log it to the console.
 
+const mainTitleElement = document.getElementById('mainTitle');
+console.log(mainTitleElement);
+
 // Exercise 2: Changing HTML Content
 // TODO: Select all paragraph elements and change the content of the second paragraph to "This paragraph has been changed."
 
+const paragraphs = document.querySelectorAll('.paragraph');
+paragraphs[1].textContent = 'This paragraph has been changed.';
+
 // Exercise 3: Changing CSS Styles
-// TODO: Select the first list item and change its text color to red
+// TODO: Select the first list item and change its text color to red and background color to light blue
+
+const firstListItem = document.querySelector('#list li');
+firstListItem.style.color = 'red';
+
 
 // Exercise 4: Adding/Removing Classes
 // TODO: Add the "highlight" class to the first paragraph
 // TODO: Remove the "highlight" class from the first paragraph after 3 seconds
 
+const firstParagraph = document.querySelector('.paragraph');
+firstParagraph.classList.add('highlight');
+setTimeout(() => {
+  firstParagraph.classList.remove('highlight');
+}, 3000);
+
 // Exercise 5: Creating and Appending Elements
 // TODO: Create a new list item with the text "Item 4" and append it to the existing list
+
+const newListItem = document.createElement('li');
+newListItem.textContent = 'Item 4';
+const list = document.getElementById('list');
+list.appendChild(newListItem);
 
 // Exercise 6: Removing Elements
 // TODO: Remove the last list item from the unordered list
