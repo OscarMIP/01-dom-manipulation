dc9c4a9b6294cbdd475762e33f5b450a7a730164
OscarMIP <oscar.mataro.epiaedu.cat>
2024-10-13 20:26:35 +0200

    Challenge completed

--- script.js
+++ script.js
@@ -23,31 +23,74 @@ firstListItem.style.color = 'red';
 // TODO: Add the "highlight" class to the first paragraph
 // TODO: Remove the "highlight" class from the first paragraph after 3 seconds
 
+
 const firstParagraph = document.querySelector('.paragraph');
 firstParagraph.classList.add('highlight');
 setTimeout(() => {
   firstParagraph.classList.remove('highlight');
 }, 3000);
 
+
 // Exercise 5: Creating and Appending Elements
 // TODO: Create a new list item with the text "Item 4" and append it to the existing list
+
 
 const newListItem = document.createElement('li');
 newListItem.textContent = 'Item 4';
 const list = document.getElementById('list');
 list.appendChild(newListItem);
 
+
 // Exercise 6: Removing Elements
 // TODO: Remove the last list item from the unordered list
+
+
+const list2 = document.getElementById('list');
+const lastListItem = list.lastElementChild;
+if (lastListItem) {
+    list.removeChild(lastListItem);
+}
+
 
 // Exercise 7: Handling Events
 // TODO: Add a click event listener to the button that displays an alert saying "Button clicked!"
 
+
+const actionButton = document.getElementById('actionButton');
+actionButton.addEventListener('click', () => {
+    alert('Button clicked!');
+});
+   
+
 // Exercise 8: Working with Input
 // TODO: Add an input event listener to the input field that displays the current input value in the result div
+
+
+const inputField = document.getElementById('inputField');
+const resultDiv = document.getElementById('result');
+inputField.addEventListener('input', () => {
+    resultDiv.textContent = inputField.value;
+});
+
 
 // Exercise 9: Toggling Visibility
 // TODO: Add a click event listener to the button that toggles the visibility of the content div
 
+
+const actionButton2 = document.getElementById('actionButton');
+const contentDiv = document.getElementById('content');
+
+actionButton.addEventListener('click', () => {
+  if (contentDiv.style.display === 'none' || contentDiv.style.display === '') {
+    contentDiv.style.display = 'block';
+  } else {
+    contentDiv.style.display = 'none';
+  }
+});
+
+
 // Exercise 10: Modifying Attributes
 // TODO: Select the input field and change its placeholder text to "Enter your name"
+
+const inputField2 = document.getElementById('inputField');
+inputField.placeholder = 'Enter your name';
